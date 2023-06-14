bf2d41a894562e5b9b4a04d4f3be3a6aea0e4e6c
yukimakura <yukimakura86@gmail.com>
2023-06-14 14:29:28 +0000

    add copy button

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,18 +7,18 @@ const Layout = ({ location, title, children }) => {
   const isRootPath = location.pathname === rootPath
   let header
 
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
+  header = (
+    <h1 className="main-heading">
+      <Link to="/">{title}</Link>
+    </h1>
+  )
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-      © {new Date().getFullYear()} yukimakura All rights reserved, Built with
+        © {new Date().getFullYear()} yukimakura All rights reserved, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
